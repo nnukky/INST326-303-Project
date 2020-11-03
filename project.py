9c224942eff308b3e5306292dba496fd6fa299a1
Jonathan Chen <jonathanchen0510@gmail.com>
2020-11-03 11:06:21 -0500

    created file

---
+++ project.py
@@ -0,0 +1,12 @@
+"""
+INST 326 Final Project
+Purpose is to create a warehouse and allow users to locate products and view other features associated with them.
+"""
+
+def Helper:
+    """
+    Purpose:
+    Attributes:
+    
+    """
+    
