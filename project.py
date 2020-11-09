03d70311fce662fcf8cbc2ae1581175f09e93323
Luis Alfaro <nnukky@gmail.com>
2020-11-09 16:16:27 -0500

    added two methods, one contains it's docstring and the other does not yet

--- project.py
+++ project.py
@@ -9,11 +9,11 @@ class Helper:
     Attributes:
     
     """
-    def __init__():
+    def __init__(self):
         """The Purpose of this method is to initilize new attributes"""
         
         
-    def categories():
+    def categories(self):
         """The purpose of this method is to help get the user to identitify 
         what item they are looking for
         
@@ -23,4 +23,19 @@ class Helper:
          """
          #Ask user if they would like to search for a specific product or choose from our list of categories
          #If user answers yes to type in specific product, take them to the product
-         #If user answers no, prompt user to pick from selected categories 
+         #If user answers no, prompt user to pick from selected categories
+         
+    def suggestions(self):
+        """
+        This method will look at items in the customer's cart and recommend them 
+        items to purchase that are frequently bought together
+        Args:
+            cart_parameter (list): A list containing items in a customer's cart
+        Raises:
+            ValueError: Value error if a suggested item is out of stock
+        """
+        
+    def cart_total(self):
+        """
+        
+        """
