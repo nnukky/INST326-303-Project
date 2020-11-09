c5effcd4318fd3ce5a67ab163c3dfe50d656d0d1
Ben Lindner <benlindner@Bens-MacBook-Pro.local>
2020-11-08 22:11:39 -0500

    Added init and categories method, need to finish making docstring

--- project.py
+++ project.py
@@ -3,10 +3,20 @@ INST 326 Final Project
 Purpose is to create a warehouse and allow users to locate products and view other features associated with them.
 """
 
-def Helper:
+class Helper:
     """
     Purpose:
     Attributes:
     
     """
-    
+    def __init__():
+        """The Purpose of this method is to initilize new attributes"""
+        
+        
+    def categories():
+        """The purpose of this method is to help get the user to identitify 
+        what item they are looking for
+        
+        Attributes:
+            
+         """
