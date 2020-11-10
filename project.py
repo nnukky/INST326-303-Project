faa38f74eb036aa2c76566ba487df69814102af0
Brigid Sax <bsax@umd.edu>
2020-11-10 13:25:20 -0500

    added attributes into class header

--- project.py
+++ project.py
@@ -8,6 +8,10 @@ class Helper:
     The purpose of this class is to help a user identify and locate items that they are looking for in our fictional warehouse.
     Attributes:
         warehouse (dict) : This is a dictionary that identifies the item name (ex: whole milk) as the key and a tuple of its attributes (ex: $2.99, 3 in stock, dairy, aisle 5) as the value.
+        cart (dict) :
+        price (float) :
+        category (str) :
+        item (str) :
     
     """
     def __init__(self):
