02712e8386545da1a33189143dcfa54efe81eca7
Brigid Sax <bsax@umd.edu>
2020-12-16 15:01:29 -0500

    more test statements

--- project_test.py
+++ project_test.py
@@ -4,12 +4,15 @@ import project as p
 from unittest import mock
 import builtins
 
-def test_categories_search():
+
+def test_categories_search(capsys):
+    captured = capsys.readouterr()
     test = p.Helper(p.create_store())
-    if specific_search == "yes" and selection==1:
-        assert test.categories_search(1) ==("iPhone 12", 999.99, "Aisle 1", "Electronics", 8)
-    if specific_search == "yes" and selection==5:
-        assert test.categories_search(25) ==("Living Room Chair", 74.99, "Aisle 8", "Furniture", 8)
+    if captured.out == "yes":
+        if specific_search == "yes" and selection==1:
+            assert test.categories_search(1) ==("iPhone 12", 999.99, "Aisle 1", "Electronics", 8)
+        if specific_search == "yes" and selection==5:
+            assert test.categories_search(25) ==("Living Room Chair", 74.99, "Aisle 8", "Furniture", 8)
 
 #def test_price_search():
     #test = p.Helper(p.create_store())
@@ -28,6 +31,7 @@ def test_cart_total():
     
 def test_narrow_categories() :
     test = p.Helper(p.create_store())
+    # happy path
     while test.narrow_categories(1) == True :
         with mock.patch("builtins.input",
                 side_effect = ["yes", "Apple", "phone"]):
@@ -37,11 +41,36 @@ def test_narrow_categories() :
             assert s == x, y
             captured = capsys.readouterr()
             assert captured.out == ""
+    # invalid inputs
+    while test.narrow_categories(1) == True :
+        with mock.patch("builtins.input",
+                side_effect = ["yes", "T-Mobile", "laptop"]):
+            s = test.narrow_categories(selection)
+            assert s == ""
+            captured = capsys.readouterr()
+            assert captured.out == (
+                "We do not have this brand in our inventory."
+                "We do not have this electronic type in our inventory."
+            )
     
-def test_item_attributes() :
-    test = p.Helper(p.create_store())
-    with mock.patch("builtins.input",
-                    side_effect = "")
+#def test_item_attributes() :
+    #test = p.Helper(p.create_store())
+    # happy path
+    #with mock.patch("builtins.input", 
+        #side_effect = "iPhone 12") :
+    # s = test.item_attributes()
+    # assert s = "iPhone 12 is $ and you can find it in AISLE in the DEPT Department. We currently have NUMBER in stock."
+    # captured capsys.readouterr()
+    # assert captured.out == ""
+    # invalid input
+    #with mock.patch("builtins.input", 
+        #side_effect = "iPhone 12") :
+    # s = test.item_attributes()
+    # assert s = Cheese Balls
+    #captured = capsys.readouterr()
+            #assert captured.out == (
+                #"This item does not exist in the store."
+            #)           
     
 def test_add_to_cart():
     test = p.Helper(p.create_store())
