f7400544b9057002723b434e52b41dd8f1daebbc
Brigid Sax <bsax@umd.edu>
2020-12-17 15:56:58 -0500

    accidental changes

--- project.py
+++ project.py
@@ -278,7 +278,7 @@ class Helper:
         for key in self.warehouse:
             if item in self.warehouse[key][0] :
                 print(f"{item} is ${self.warehouse[key][1]} with ID {self.warehouse[key][5]} and you can find it in {self.warehouse[key][2]} in the {self.warehouse[key][3]} Department. We currently have {self.warehouse[key][4]} in stock.")
-                break
+                break 
             else: 
                 raise ValueError("This item does not exist in the store.")
         
